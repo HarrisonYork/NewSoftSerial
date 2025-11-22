3e8ff6dbdda14e513aa89c027332d6ab73f9c2e9
HarrisonYork <harrisonyork03@gmail.com>
2025-11-22 18:25:02 -0500

    Update and rename NewSoftSerialY.h to NewSoftSerial.h

--- NewSoftSerial.h
+++ NewSoftSerial.h
@@ -25,8 +25,8 @@ The latest version of this library can always be found at
 http://arduiniana.org.
 */
 
-#ifndef NewSoftSerialY_h
-#define NewSoftSerialY_h
+#ifndef NewSoftSerial_h
+#define NewSoftSerial_h
 
 #include <inttypes.h>
 #include "Print.h"
